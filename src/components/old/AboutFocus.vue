05cb7156c294bcfef6d657a6300c2a45d1527ac4
lIlIllIIIIl <alexis.kessab@orange.fr>
2025-04-26 14:41:26 +0900

    feat: create/update old portfolio pages

---
+++ src/components/old/AboutFocus.vue
@@ -0,0 +1,71 @@
+<script setup>
+import oldInterests from '@/store/oldInterests'
+
+import Cross from '@/assets/icons/Cross.vue'
+
+const props = defineProps(['focus'])
+</script>
+
+<template>
+  <div class="about-focus">
+    <div class="focus-title">
+      <h1>{{ props.focus.title }}</h1>
+      <Cross />
+    </div>
+
+    <div class="focus-informations">
+      <img :src="`/src/assets/images/interests/${props.focus.image}.jpg`" />
+
+      <div class="focus-more">
+        <p>
+          {{ props.focus.desc }}
+        </p>
+
+        <div>
+          <img
+            v-for="interest in oldInterests"
+            :key="interest.title"
+            :src="`/src/assets/images/interests/${interest.image}.jpg`"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about-focus {
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+
+  .focus-title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 16px;
+  }
+
+  .focus-informations {
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+    //
+    height: 100%;
+
+    img {
+      max-height: 380px;
+    }
+
+    .focus-more {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      img {
+        max-height: 120px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/old/AboutPage.vue
+++ src/pages/old/AboutPage.vue
@@ -1,7 +1,74 @@
-<script setup></script>
+<script setup>
+import AboutFocus from '@/components/old/AboutFocus.vue'
+import oldInterests from '@/store/oldInterests'
+
+import { gsap } from 'gsap'
+
+import Splitting from 'splitting'
+
+const currentFocus = ref(null)
+
+function focusInterest(interest) {
+  const aboutContainer = document.querySelector('.about-container')
+
+  const aboutFocus = document.querySelector('.about-focus')
+
+  currentFocus.value = interest
+
+  Splitting()
+
+  const tl = gsap.timeline()
+
+  tl.to(aboutContainer, {
+    display: 'none',
+    duration: 0,
+  })
+
+  tl.to(aboutFocus, {
+    display: 'flex',
+    duration: 0,
+  })
+}
+</script>
 
 <template>
-  <div class="about-container">about</div>
+  <div class="about-container">
+    <h1>About me?</h1>
+
+    <div class="interests-container">
+      <img
+        v-for="interest in oldInterests"
+        :key="interest.title"
+        :src="`/src/assets/images/interests/${interest.image}.jpg`"
+        @click="focusInterest(interest)"
+      />
+    </div>
+  </div>
+
+  <AboutFocus :focus="currentFocus ?? oldInterests.ski" />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.about-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 24px;
+
+  .interests-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 8px;
+
+    img {
+      max-height: 120px;
+    }
+  }
+}
+
+.about-focus {
+  display: none;
+}
+</style>
