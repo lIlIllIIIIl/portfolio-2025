eaf5513c8fd0136d7557264eb87c9e3950a47605
lIlIllIIIIl <alexis.kessab@orange.fr>
2025-06-04 23:34:13 +0800

    feat: update homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,12 @@
 <script setup>
-import Project from '@/components/atoms/Project.vue'
-import projects from '@/store/projects.json'
+// import Project from '@/components/atoms/Project.vue'
+// import projects from '@/store/projects.json'
 </script>
 
 <template>
   <section class="homepage-container">
-    <div>
-      <span>Alexis Kessab</span>
+    <div class="homepage-presentation">
+      <span class="name">Alexis Kessab</span>
       <span>Fullstack Developer</span>
     </div>
   </section>
@@ -14,5 +14,20 @@ import projects from '@/store/projects.json'
 
 <style lang="scss" scoped>
 .homepage-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .homepage-presentation {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.4vh;
+
+    .name {
+      color: white;
+    }
+  }
 }
 </style>
